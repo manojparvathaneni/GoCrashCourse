425cb9a17cb7f360844722fab6756ebf6d1cf80e
Manoj Parvathaneni <manoj@manojs-mac-mini-pro.local>
2024-04-24 06:36:18 -0400

    variable and function declaration added

--- section1/hello_world/main.go
+++ section1/hello_world/main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
+
 	fmt.Println("Hello, World!")
+	sayHelloWorld("Hello world, from a new function")
+
+	//variables
+	//version 1 of declaration
+	var message1 string
+	message1 = "Hello from message1"
+	sayHelloWorld(message1)
+
+	//version 2 of declaration
+	message2 := "Hello from message2"
+	sayHelloWorld(message2)
+}
+
+func sayHelloWorld(whatToSay string) {
+	fmt.Println(whatToSay)
 }
