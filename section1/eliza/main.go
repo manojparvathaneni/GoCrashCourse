ae03951eab5aa56a25dd0f8c1d7d7c5ffdf3766c
ManojParvathaneni <gopher@godevsrv.pn5haonp155ephfsnsdkio51af.cx.internal.cloudapp.net>
2024-05-07 06:33:40 +0000

    eliza updated with userinput and ability to quit

--- section1/eliza/main.go
+++ section1/eliza/main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"eliza/doctor"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+
 	whatToSay := doctor.Intro()
+
 	fmt.Println(whatToSay)
+
+	for {
+		fmt.Print("-> ")
+		userInput, _ := reader.ReadString('\n')
+
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		if userInput == "quit" {
+			break
+		}
+		response := doctor.Response(userInput)
+		fmt.Println(response)
+	}
+
 }
