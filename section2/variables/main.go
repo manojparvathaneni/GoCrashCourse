2fbcfa6a11e1895df3c4fe41df2fd484b41c8272
Manoj Parvathaneni <dscatwork@outlook.com>
2024-05-14 07:17:40 -0400

    created the number guessing game

--- section2/variables/main.go
+++ section2/variables/main.go
@@ -1,11 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const prompt = "and Press ENTER when ready"
 
 func main() {
 
-	var firstNumber int
-	firstNumber = 2
+	firstNumber := 2
+	secondNumber := 5
+	subtract := 7
+	var answer int
 
-	fmt.Println(firstNumber)
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Guess the number game.")
+	fmt.Println("----------------------")
+	fmt.Println()
+
+	fmt.Println("Think of a number between 1 and 10", prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Multiply your number by", firstNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now Multiply the result by", secondNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Divide the result by the number you originally thought of", prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now subtract", subtract, prompt)
+	reader.ReadString('\n')
+
+	answer = firstNumber*secondNumber - subtract
+	fmt.Println("The answer is", answer)
 }
